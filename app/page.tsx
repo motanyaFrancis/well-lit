423ad5de59015c0cc650a6b087b53f8992415a98
motanyafrancis <francismotanya@outlook.com>
2025-02-24 20:24:53 +0300

    bg changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Navigation from "./components/Navigation";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-[url(/images/background.jpg)] bg-cover bg-cente font-[family-name:var(--font-geist-sans)]r">
+      <div className="min-h-screen bg-[url(/images/background.jpg)] bg-cover bg-center font-[family-name:var(--font-geist-sans)]r">
         <Navigation />
         
 
